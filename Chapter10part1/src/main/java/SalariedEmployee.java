449dc589216e10b25d7d27c7fd2f8af9c835d7cc
Taiyeaba Shams <tayebashams@gmail.com>
2024-09-28 11:32:42 +0600

    Add files via upload

---
+++ Chapter10part1/src/main/java/SalariedEmployee.java
@@ -0,0 +1,45 @@
+ public class SalariedEmployee extends Employee 
+{
+   private double weeklySalary;
+   
+ // constructor
+   public SalariedEmployee(String firstName, String lastName, 
+     String socialSecurityNumber, double weeklySalary)
+   {
+      super(firstName, lastName, socialSecurityNumber); 
+
+      if (weeklySalary < 0.0)
+         throw new IllegalArgumentException(
+            "Weekly salary must be >= 0.0");
+ 
+      this.weeklySalary = weeklySalary;
+   } 
+
+   // set salary
+   public void setWeeklySalary(double weeklySalary)
+    {
+       if (weeklySalary < 0.0)
+         throw new IllegalArgumentException(
+            "Weekly salary must be >= 0.0");
+        this.weeklySalary = weeklySalary;
+    } 
+
+   // return salary
+   public double getWeeklySalary()
+   {
+      return weeklySalary;
+   } 
+
+ 
+  @Override                                                    
+   public double getPaymentAmount()                                  
+{                                                                 
+   return getWeeklySalary(); 
+}
+     @Override
+      public String toString()                                     
+  {                                                            
+    return String.format("salaried employee: %s%n%s: $%,.2f",
+          super.toString(), "weekly salary", getWeeklySalary());
+  } 
+ }
